0fb4aa215d81e8ac89b0627aeff3057608897996
emunimu <muhammademan1921@gmail.com>
2026-06-07 17:39:18 +0500

    Remove CORS middleware from server.js
    Removed CORS middleware from server configuration.

--- server/server.js
+++ server/server.js
@@ -19,9 +19,6 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors()
-);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
